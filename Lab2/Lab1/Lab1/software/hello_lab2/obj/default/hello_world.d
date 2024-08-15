a76c0f9451b0360f69bda29421d7a864f15791dd
lthw148 <127149870+lthw148@users.noreply.github.com>
2024-08-15 16:02:37 +1200

    sdafsd

---
+++ Lab2/Lab1/Lab1/software/hello_lab2/obj/default/hello_world.d
@@ -0,0 +1,17 @@
+obj/default/hello_world.o: hello_world.c ../hello_lab2_bsp/system.h \
+ ../hello_lab2_bsp/linker.h \
+ ../hello_lab2_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../hello_lab2_bsp//HAL/inc/io.h ../hello_lab2_bsp//HAL/inc/alt_types.h \
+ SCC_Charts.h
+
+../hello_lab2_bsp/system.h:
+
+../hello_lab2_bsp/linker.h:
+
+../hello_lab2_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../hello_lab2_bsp//HAL/inc/io.h:
+
+../hello_lab2_bsp//HAL/inc/alt_types.h:
+
+SCC_Charts.h:
---
+++ Lab2/Lab1/Lab1/software/hello_lab2/obj/default/lab2.1.d
@@ -0,0 +1,32 @@
+obj/default/lab2.1.o: lab2.1.c ../hello_lab2_bsp/system.h \
+ ../hello_lab2_bsp/linker.h \
+ ../hello_lab2_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../hello_lab2_bsp//HAL/inc/io.h ../hello_lab2_bsp//HAL/inc/alt_types.h \
+ ../hello_lab2_bsp//HAL/inc/alt_types.h \
+ ../hello_lab2_bsp//HAL/inc/sys/alt_irq.h \
+ ../hello_lab2_bsp//HAL/inc/nios2.h \
+ ../hello_lab2_bsp//HAL/inc/sys/alt_alarm.h \
+ ../hello_lab2_bsp//HAL/inc/sys/alt_llist.h \
+ ../hello_lab2_bsp//HAL/inc/priv/alt_alarm.h
+
+../hello_lab2_bsp/system.h:
+
+../hello_lab2_bsp/linker.h:
+
+../hello_lab2_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../hello_lab2_bsp//HAL/inc/io.h:
+
+../hello_lab2_bsp//HAL/inc/alt_types.h:
+
+../hello_lab2_bsp//HAL/inc/alt_types.h:
+
+../hello_lab2_bsp//HAL/inc/sys/alt_irq.h:
+
+../hello_lab2_bsp//HAL/inc/nios2.h:
+
+../hello_lab2_bsp//HAL/inc/sys/alt_alarm.h:
+
+../hello_lab2_bsp//HAL/inc/sys/alt_llist.h:
+
+../hello_lab2_bsp//HAL/inc/priv/alt_alarm.h:
